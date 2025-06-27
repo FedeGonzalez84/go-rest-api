6e328f15a9188c457364f36dd287fd51562df807
Fede Gonzalez <fedegonzalez@example.com>
2025-06-27 17:33:29 -0300

    Entire api-rest make in GO

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/FedeGonzalez84/go-rest-api/db"
+	"github.com/FedeGonzalez84/go-rest-api/routes"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	db.InitDB()
+	server := gin.Default()
+
+	routes.RegisterRoutes(server)
+
+	server.Run(":8080")
 }
